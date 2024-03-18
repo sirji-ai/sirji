6e0e02396da5067d0a61d87ed103d935a791ec99
VRDighe <vaibhav@plgworks.com>
2024-03-18 18:25:55 +0530

    Updated the logger module to implement the singleton pattern

--- sirji/logger.py
+++ sirji/logger.py
@@ -1,17 +1,26 @@
 import os
 import logging
 
-def setup_logger(file_name, log_level=logging.INFO):
-    # Create a folder named "log" if it doesn't exist
+class LoggerSingleton:
+    _instances = {}
 
-    log_folder = os.path.join("workspace", "logs")
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
+    def __new__(cls, file_name, log_level=logging.INFO):
+        if file_name not in cls._instances:
+            cls._instances[file_name] = super().__new__(cls)
+            cls._instances[file_name]._setup_logger(file_name, log_level)
+        return cls._instances[file_name]
 
-    # Set up logging
-    log_file_path = os.path.join(log_folder, file_name)
-    logging.basicConfig(filename=log_file_path, level=log_level,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+    def _setup_logger(self, file_name, log_level):
+        # Create a folder named "log" if it doesn't exist
+        log_folder = os.path.join("workspace", "logs")
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
 
-    logger = logging.getLogger()
-    return logger
+        # Set up logging
+        log_file_path = os.path.join(log_folder, file_name)
+        logging.basicConfig(filename=log_file_path, level=log_level,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger()
+
+def get_logger(file_name, log_level=logging.INFO):
+    return LoggerSingleton(file_name, log_level).logger
