fdf224b56497d925f4329eb74dca9bc092e5128f
Kiran Kamalakar <kamalakarkiran07@gmail.com>
2024-03-21 10:44:13 +0530

    Added filepath in logger

--- sirji/tools/logger/logger.py
+++ sirji/tools/logger/logger.py
@@ -15,25 +15,32 @@ _default_log_level = _log_level_str_to_const.get(os.environ.get("SIRJI_LOG_LEVEL
 
 # Singleton class to create loggers
 class LoggerSingleton:
+
     def __init__(self, file_name, log_level):
         self.logger = self._setup_logger(file_name, log_level)
-
+    
+    def _log_folder(self):
+        return os.path.join("workspace", "logs")
+    
+    def _log_file_path(self, file_name):
+        return os.path.join(self._log_folder(), file_name)
+        
     def _setup_logger(self, file_name, log_level):
         # Create a folder named "logs" if it doesn't exist
-        log_folder = os.path.join("workspace", "logs")
-        if not os.path.exists(log_folder):
-            os.makedirs(log_folder)
-
-        # Set up logging
-        log_file_path = os.path.join(log_folder, file_name)
+        if not os.path.exists(self._log_folder()):
+            os.makedirs(self._log_folder())
+        
         logger = logging.getLogger(file_name)
         logger.setLevel(log_level)
         if not logger.handlers:
             # Ensure no duplicate handlers
-            file_handler = logging.FileHandler(log_file_path)
+            file_handler = logging.FileHandler(self._log_file_path(file_name))
             formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
             file_handler.setFormatter(formatter)
             logger.addHandler(file_handler)
+        
+        logger.filepath = self._log_file_path(file_name)
+
         return logger
 
 # Manager class to create and manage loggers
