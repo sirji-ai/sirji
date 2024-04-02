1fa53fa4076a942435c35d3cc80a9f722bc9141a
VRDighe <vaibhav@plgworks.com>
2024-04-02 18:37:19 +0530

    Removed the tools and config from sirji

--- sirji/tools/logger/__init__.py
+++
@@ -1,8 +0,0 @@
-from .logger import LoggerManager
-
-coder = LoggerManager().coder
-researcher = LoggerManager().researcher
-planner = LoggerManager().planner
-executor = LoggerManager().executor
-sirji = LoggerManager().sirji
-user = LoggerManager().user
