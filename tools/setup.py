5e1da325257e590fd581e00220803394e9c6c4db
kedarchandrayan <kedarchandrayan@gmail.com>
2024-04-05 21:15:55 +0530

    Fixing README heart on PyPI

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-tools',
-    version='0.0.6',
+    version='0.0.7',
     author='Sirji',
     description='Crawler and search tools used by Sirji.',
     license='MIT',
