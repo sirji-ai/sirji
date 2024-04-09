fa3a77dda445de582fbb1a86c025c17fa074157a
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-04-09 15:34:15 +0530

    Updated settings modal UI

--- sirji/vscode-extension/src/views/chat/chat.js
+++ sirji/vscode-extension/src/views/chat/chat.js
@@ -361,14 +361,16 @@ function toggleProgressTextColor() {
 }
 
 function convertNumber(number) {
-  if (number >= 1000000) {
-    return Math.round(number / 100000) / 10 + 'M';
+  if (number >= 1000000000) {
+      return (Math.round(number / 10000000) / 100) + 'B';
+  } else if (number >= 1000000) {
+      return (Math.round(number / 100000) / 10) + 'M';
   } else if (number >= 100000) {
-    return Math.round(number / 10000) / 100 + 'M';
+      return (Math.round(number / 10000) / 100) + 'M';
   } else if (number >= 1000) {
-    return Math.round(number / 100) / 10 + 'K';
+      return (Math.round(number / 100) / 10) + 'K';
   } else {
-    return number.toString();
+      return number.toString();
   }
 }
 
