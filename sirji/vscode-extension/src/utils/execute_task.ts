5a57af749c5ce21badf3b0943a934e1e660d232f
Daksh Bhardwaj <daksh@truesparrow.com>
2024-04-09 13:50:25 +0530

    updated checks in execute task

--- sirji/vscode-extension/src/utils/execute_task.ts
+++ sirji/vscode-extension/src/utils/execute_task.ts
@@ -14,7 +14,7 @@ async function checkForChanges(filePath: string, previousContent: string): Promi
 }
 
 function constructResponse(isRunning: Boolean, tempFilePath: string, tempFileContent: string): string {
-  let response = isRunning ? 'Execute Command still running.' : 'Execute command complete.';
+  let response = isRunning ? 'Execute Command still running.' : 'Execute command complete. ';
   response += `Command execution output from ${tempFilePath}\n`;
   response += `${tempFileContent}`;
   return response;
@@ -26,7 +26,7 @@ export async function executeTask(command: string, workspaceRootPath: string): P
     const tempFilePath = path.join(workspaceRootPath, tempFileName);
     let tempFileContent: string;
 
-    command = `(${command}) > "${tempFilePath}" 2>&1`;
+    command = `(${command}) 2>&1 | tee "${tempFilePath}"`;
 
     const shellExecution = new vscode.ShellExecution(command);
     const taskDefinition = { type: 'shell' };
@@ -41,17 +41,24 @@ export async function executeTask(command: string, workspaceRootPath: string): P
     try {
       const execution = await vscode.tasks.executeTask(task);
 
+      console.log(`executeTask new task ${command}:`, { isTaskExecutionInProgress, hasReturnTaskExecutionResponse, tempFileName });
+
       disposables.push(
         vscode.tasks.onDidStartTask((event) => {
+          console.log(`executeTask inside onDidStartTask ${command}:`, event);
           if (event.execution.task === task) {
             isTaskExecutionInProgress = true;
+            console.log(`executeTask setting setInterval ${command}:`, { isTaskExecutionInProgress, hasReturnTaskExecutionResponse, tempFileName });
             checkTaskPeriodically = setInterval(async function () {
               const response = await checkForChanges(tempFilePath, tempFileContent);
+
+              console.log(`executeTask setInterval executed ${command}:`, { isTaskExecutionInProgress, hasReturnTaskExecutionResponse, tempFileName });
 
               tempFileContent = response.currentContent;
               if (!response.hasChanges && !hasReturnTaskExecutionResponse) {
                 hasReturnTaskExecutionResponse = true;
-
+                console.log(`executeTask setInterval return ${command}:`, { isTaskExecutionInProgress, hasReturnTaskExecutionResponse, tempFileName });
+                clearInterval(checkTaskPeriodically);
                 return resolve(constructResponse(isTaskExecutionInProgress, tempFilePath, tempFileContent));
               }
             }, 30000);
@@ -60,13 +67,16 @@ export async function executeTask(command: string, workspaceRootPath: string): P
       );
 
       disposables.push(
-        vscode.tasks.onDidEndTaskProcess(async (event) => {
+        vscode.tasks.onDidEndTask(async (event) => {
+          console.log(`executeTask inside onDidEndTaskProcess ${command}:`, event);
           isTaskExecutionInProgress = false;
-          clearTimeout(checkTaskPeriodically);
+          clearInterval(checkTaskPeriodically);
           if (!hasReturnTaskExecutionResponse) {
             const response = await checkForChanges(tempFilePath, tempFileContent);
             tempFileContent = response.currentContent;
             hasReturnTaskExecutionResponse = true;
+            console.log(`executeTask onDidEndTaskProcess return ${command}:`, { isTaskExecutionInProgress, hasReturnTaskExecutionResponse, tempFileName });
+            disposables.forEach((d) => d.dispose());
             return resolve(constructResponse(isTaskExecutionInProgress, tempFilePath, tempFileContent));
           }
         })
@@ -74,8 +84,6 @@ export async function executeTask(command: string, workspaceRootPath: string): P
     } catch (error) {
       console.error(`Failed to execute task: ${error}`);
       throw error;
-    } finally {
-      disposables.forEach((d) => d.dispose());
     }
   });
 }
