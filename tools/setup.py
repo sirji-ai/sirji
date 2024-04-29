874426f038ac065748d5a73975b1daa013d9bc2f
kedarchandrayan <kedarchandrayan@gmail.com>
2024-04-29 14:51:26 +0530

    version updates.

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-tools',
-    version='0.0.10',
+    version='0.0.11',
     author='Sirji',
     description='Crawler and search tools used by Sirji.',
     license='MIT',
