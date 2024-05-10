5949f26e8bdf2301017a6e3a6372759bc265d52f
kedarchandrayan <kedarchandrayan@gmail.com>
2024-05-10 21:48:13 +0530

    version update

--- messages/setup.py
+++ messages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-messages',
-    version='0.0.16',
+    version='0.0.17',
     author='Sirji',
     description='Sirji messaging protocol implementation to create, validate and parse messages.',
     license='MIT',
