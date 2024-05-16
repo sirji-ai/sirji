2cc76f71c01f4860b0372081ddf8077aa5a1fa52
Kedar Chandrayan <kedarchandrayan@gmail.com>
2024-05-16 17:55:39 +0530

    Update setup.py

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-tools',
-    version='0.0.11',
+    version='0.0.12',
     author='Sirji',
     description='Crawler and search tools used by Sirji.',
     license='MIT',
