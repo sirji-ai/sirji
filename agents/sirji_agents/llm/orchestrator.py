e5e0b6501f467be5fd0f01e306abf174952b22c7
Daksh Bhardwaj <daksh@truesparrow.com>
2024-06-06 20:06:21 +0530

    updated INVOKE_AGENT to Invoke agent and Scratch pad to Scratchpad

--- agents/sirji_agents/llm/orchestrator.py
+++ agents/sirji_agents/llm/orchestrator.py
@@ -118,7 +118,7 @@ class Orchestrator():
         
         pseudo_code = textwrap.dedent(f"""
             Pseudo code which you must follow:
-                1. INVOKE_AGENT RECIPE_SELECTOR to get the recipe selected from the available recipes by SIRJI_USER and then store it in Agent Output Folder.
+                1. Invoke agent RECIPE_SELECTOR to get the recipe selected from the available recipes by SIRJI_USER and then store it in Agent Output Folder.
                 2. Read the selected recipe from the Agent Output Folder.
                 3. Loop over the prescribed tasks in the selected recipe and invoke the agent specified in the recipe alongside the current task, explaining the task in the BODY of the invocation.
             """)
