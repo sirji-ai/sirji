909ff6008a3d98cc5490229ecb86af9178b4943e
Daksh Bhardwaj <daksh@truesparrow.com>
2024-06-12 18:23:11 +0530

    updated python package verson

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-tools',
-    version='0.0.12',
+    version='0.0.13',
     author='Sirji',
     description='Crawler and search tools used by Sirji.',
     license='MIT',
