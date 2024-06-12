48c1e3e2dc1108d65d249f3638ef38236cacd354
Daksh Bhardwaj <daksh@truesparrow.com>
2024-06-12 18:56:23 +0530

    fixed failing test cases

--- messages/setup.py
+++ messages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sirji-messages',
-    version='0.0.25',
+    version='0.0.26',
     author='Sirji',
     description='Sirji messaging protocol implementation to create, validate and parse messages.',
     license='MIT',
